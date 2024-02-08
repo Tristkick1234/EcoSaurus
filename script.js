d3710abab2c2b6a5985b3d10c0b7e9f86f4ec5f2
tristkick1234 <Tristkick@outlook.com>
2024-02-07 21:38:45 -0500

    Website overhaul

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 window.addEventListener('scroll', function(event) {
     const navigationBar = document.getElementById('nav');
     
-    if (window.scrollY > (window.innerHeight)*0.9) {
+    if (window.scrollY > (window.innerHeight)*0.3) {
     navigationBar.style.backgroundColor="#7f9f5de0";
     } else {
     navigationBar.style.backgroundColor="#7f9f5d18";
