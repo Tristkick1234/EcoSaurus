f47252144777b0d9f503243a4a4125f3c9594400
tristkick1234 <tristkick@outlook.com>
2024-02-18 08:37:11 -0500

    Completely Responsive

--- script.js
+++ script.js
@@ -7,12 +7,14 @@
 
 //changing navbar color after 90% of the user viewport
 
-window.addEventListener('scroll', function(event) {
-    const navigationBar = document.getElementById('nav');
-    
-    if (window.scrollY > (window.innerHeight)*0.3) {
-    navigationBar.style.backgroundColor="#7f9f5de0";
-    } else {
-    navigationBar.style.backgroundColor="#7f9f5d18";
-    }
-   });
+if (window.innerWidth > 1000) {
+    window.addEventListener('scroll', function(event) {
+        const navigationBar = document.getElementById('nav');
+        
+        if (window.scrollY > (window.innerHeight)*0.3) {
+            navigationBar.style.backgroundColor="#7f9f5de0";
+        } else {
+            navigationBar.style.backgroundColor="#7f9f5d18";
+        }
+    });
+}
